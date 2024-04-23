519de59071b7b6718f76c6530b49fdb43b6a17ee
C0A23122 <c0a23122e6@edu.teu.ac.jp>
2024-04-23 15:12:46 +0900

    練習3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,4 +1,5 @@
 import os
+import random
 import sys
 import pygame as pg
 
@@ -10,6 +11,17 @@ DELTA = {pg.K_UP: (0, -5),
          pg.K_RIGHT: (5, 0)}
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+def check_bound(obj_rct):
+    """こうかとんRect、または爆弾Rectの画面内外判定用の関数
+    引数:こうかとんRect,または爆弾Rect
+    戻り値：横方向判定結果、縦方向判定結果（True:画面内/False:画面外）"""
+    yoko, tate = True, True
+    if obj_rct.right < 0 or WIDTH < obj_rct.left:
+        yoko = False
+    if obj_rct.top < 0 or HEIGHT < obj_rct.bottom:
+        tate = False
+    return yoko, tate        
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -18,8 +30,17 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 2.0)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 900, 400
+    bd_img = pg.Surface((20, 20))
+    bd_img.set_colorkey((0, 0, 0))
+    pg.draw.circle(bd_img, (255, 0, 0), (10, 10), 10)
+    bd_rct = bd_img.get_rect()
+    bd_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
+
     clock = pg.time.Clock()
     tmr = 0
+    vx = +5
+    vy = +5
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
@@ -32,6 +53,9 @@ def main():
             if key_lst[k]:
                 sum_mv[0] += v[0]
                 sum_mv[1] += v[1]
+        kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
 
         #if key_lst[pg.K_UP]:
         #    sum_mv[1] -= 5
@@ -41,8 +65,15 @@ def main():
         #    sum_mv[0] -= 5
         #if key_lst[pg.K_RIGHT]:
         #    sum_mv[0] += 5
-        kk_rct.move_ip(sum_mv)
+        bd_rct.move_ip(vx, vy)
+        screen.blit(bd_img, bd_rct)
         screen.blit(kk_img, kk_rct)
+        yoko, tate = check_bound(bd_rct)
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1
+        
         pg.display.update()
         tmr += 1
         clock.tick(50)
